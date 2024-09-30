058ddf51bb8d38261b0ca00cbe5f5e98a7c26232
Jason Prasad <jasongprasad@gmail.com>
2024-09-30 11:11:53 -0400

    WIP iOS Previews

---
+++ ios/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ios",
+    platforms: [
+        .iOS(.v18),
+    ],
+    products: [
+        .library(name: "Previews", targets: ["Previews"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/divkit/divkit-ios", from: "30.20.0")
+    ],
+    targets: [
+        .target(
+            name: "Previews",
+            dependencies: [
+                .product(name: "DivKit", package: "divkit-ios")
+            ]
+        ),
+    ]
+)
---
+++ ios/Sources/Previews/DivKit.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    Card()
+}
